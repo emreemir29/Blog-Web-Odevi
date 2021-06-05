a5073b0e8185b6a33db038a1c6571ce1ecdcb2ec
emreemir29 <72244860+emreemir29@users.noreply.github.com>
2021-06-05 14:19:32 +0300

    Tarih Web Sitesi
    Tarih/Blog web sitesi

---
+++ Blog/include/nav.php
@@ -0,0 +1,79 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <img src="img/logo.png" class="logo">
+    <a class="navbar-brand fst-italic baslik" href="index.html">Tarih Günlüğü</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse navv" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="hakkimizda.html">Hakkımızda</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            OrtaÇağ
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="gokturkler.html">Göktürkler</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="karahanlilar.html">Karahanlılar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="pecenekler.html">Peçenek Hanlığı</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="memlukler.html">Memlükler</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="kipcaklar.html">Deşt-i Kıpçak</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="selcuklu.html">Büyük Selçuklu</a></li> 
+          </ul>
+        </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Yeni Çağ
+           </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="kazak-hanligi.html">Kazak Hanlığı</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="safevi-devleti.html">Safevi Devleti</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="babur-devleti.html">Babür Devleti</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="buhara-emirligi.html">Buhara Emirliği</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="baku-hanligi.html">Bakü Hanlığı</a></li> 
+          </ul>
+        </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            YakınÇağ
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="turkiye.html">Türkiye Cumhuriyeti</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="azerbaycan.html">Azerbaycan Cumhuriyeti</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="kazakistan.html">Kazakistan Cumhuriyeti</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="macaristan.html">Macaristan Cumhuriyeti</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="dogu-turkistan.html">Doğu Türkistan</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="kktc.html">KKTC</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="iletisim.html">İletişim</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Ara" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Ara</button>
+      </form>
+    </div>
+  </div>
+</nav>
